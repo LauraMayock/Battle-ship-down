2a7c54b56d213af1cbb91c23ba69156d24629b9b
LauraMayock <mcfadden.laura@yahoo.com>
2022-06-22 14:01:20 +0000

    add code for when user add guess

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+//Declare variables needed
+
+let location1;
+let location2;   //3 varibales in which the computers ship will reside
+let location3;
+let guess;    // A variable to hold the users guess
+let hits = 0;  // A variable to hold the hits 
+let guesses = 0;
+let isSunk = "false";
+
+// when a cell is clicked, it checks if the ship is there and updates the score if it is
+if(guess != location1, location2, location3) {
+        alert("Target missed, Try again")
+    } else if (guess == location1, location2, location3){
+        guesses = guesses + 1;
+        hits = hits + 1;
+        alert("You sank my battleship!");
+        }
+    
+
+
+
+
+    }
+}
